f1089171191455176b6a4708f34ae2c38fe6404b
Chase Southard <chase.southard@gmail.com>
2011-04-18 15:44:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinup::Application.config.secret_token = '1bd7079767752524acfd65994f29b79868c5efb6551ed8d9d4c87fa4ec320968ea08813a48a16da297d6cd758784028656f176eb5d6309480a7e6454512e6cf5'
